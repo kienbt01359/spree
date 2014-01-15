0686dada6e1857895c62d61a9ade144c8ca22008
kien <kien@kien-laptop.(none)>
2014-01-15 09:15:22 +0700

    Simple Sale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystore::Application.config.secret_key_base = 'd7fb01d22f7ac8ba98d4758aff043b0c2e6be3d59facfc4b0e54abb1d5b3e6b38dad59078a771f4c434d11cb5d77b3858fb00ba2cfce8930ac179ce419ae2210'
